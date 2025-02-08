e4e7899c3fbc3588619201fd5d46d3061b47de87
alashchev17 <andrew.lashchev15@gmail.com>
2025-02-08 19:47:11 +0100

    chore: PM2 ecosystem file creation

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'brewery-aggregator',
+      cwd: '/var/www/brewery-aggregator-remix-app',
+      script: '/usr/bin/npm',
+      args: 'run start',
+      env: {},
+    },
+  ],
+}
